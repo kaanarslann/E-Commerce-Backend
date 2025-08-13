828b5a65d81c35284cf30b4cf94a99834957db3e
kaanarslann <kaanarslan_hp@hotmail.com>
2025-08-13 17:54:13 +0300

    adds repositories

--- src/main/java/com/example/ecommerce/entity/Store.java
+++ src/main/java/com/example/ecommerce/entity/Store.java
@@ -1,4 +1,57 @@
 package com.example.ecommerce.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "store", schema = "ecommerce")
 public class Store {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotEmpty
+    @NotNull
+    @NotBlank
+    @Size(max = 150)
+    @Column(name = "store_name")
+    private String storeName;
+
+    @NotNull
+    @NotEmpty
+    @NotBlank
+    @Size(max = 50)
+    @Column(name = "store_phone")
+    private String storePhone;
+
+    @NotNull
+    @NotEmpty
+    @NotBlank
+    @Size(max = 50)
+    @Column(name = "store_tax_id")
+    private String storeTaxId;
+
+    @NotNull
+    @NotEmpty
+    @NotBlank
+    @Size(max = 100)
+    @Column(name = "store_bank_account")
+    private String storeBankAccount;
+
+    @Column(name = "created_at")
+    @CreatedDate
+    private LocalDate createdAt;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
---
+++ src/main/java/com/example/ecommerce/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.example.ecommerce.repository;
+
+import com.example.ecommerce.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
